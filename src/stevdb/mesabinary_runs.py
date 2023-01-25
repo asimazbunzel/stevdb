c0ea6255b055d61624b35691beabf933eb72f7fe
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-01-25 16:20:33 -0300

    MESAbinary can now save initial & final conditions of a MESArun into different tables of the database

--- src/stevdb/mesabinary_runs.py
+++ src/stevdb/mesabinary_runs.py
@@ -3,25 +3,19 @@ Module that manages a set of MESAbinary simulations
 """
 
 import glob
-
-# import os
+import os
 import re
-
-# import sqlite3
 import sys
-
-# from collections import OrderedDict
+import time
 from pathlib import Path
-
-# from pathlib import PosixPath
 from typing import Union
 
+from .io.database import create_database
+from .io.database import insert_run_into_database
 from .io.io import load_yaml
 from .io.io import progress_bar
 from .io.logger import logger
 from .mesa import MESArun
-
-# import numpy as np
 
 
 class MESAbinaryGrid(object):
@@ -75,6 +69,13 @@ class MESAbinaryGrid(object):
 
         self.runs = self._get_list_of_models()
 
+        # to overwrite database file, remove it first
+        if self.overwrite_database:
+            try:
+                os.remove(database_name)
+            except Exception:
+                logger.info("not going to remove database. file does not exist")
+
     def _get_list_of_models(self) -> list:
         """List models to summarize
         Method that checks for the number of simulations to make a summary and returns the complete folder path
@@ -115,11 +116,19 @@ class MESAbinaryGrid(object):
     def create_summary(self):
         """Create a summary of runs"""
 
+        # this flag helps to create database just once at the start
+        doing_first_model_of_summary = True
+
         for k, run in enumerate(self.runs):
+
+            # output a nice progress bar in the terminal
             right_msg = f" {k+1}/{len(self.runs)} done"
             progress_bar(k, len(self.runs), left_msg="creating summary", right_msg=right_msg)
 
             logger.debug(f"inspecting folder: `{run.split('/')[-2]}`")
+
+            # (tstart) to control amount of time of loading and processing MESA output
+            tstart = time.time()
 
             name = run.split("/")[-2]
             self.MESAsummary = MESArun(
@@ -132,34 +141,70 @@ class MESAbinaryGrid(object):
 
             # check if simulation has actual MESA output, else do not try to make a summary of them
             if self.MESAsummary.should_have_mesabinary and not self.MESAsummary.have_mesabinary:
-                logger.info("simulation does not have MESAbinary output. skipping it")
+                logger.info(" simulation does not have MESAbinary output. skipping it")
                 continue
 
             if self.MESAsummary.should_have_mesastar1 and not self.MESAsummary.have_mesastar1:
-                logger.info("simulation does not have MESAstar1 output. skipping it")
+                logger.info(" simulation does not have MESAstar1 output. skipping it")
                 continue
 
             if self.MESAsummary.should_have_mesastar2 and not self.MESAsummary.have_mesastar2:
-                logger.info("simulation does not have MESAstar2 output. skipping it")
+                logger.info(" simulation does not have MESAstar2 output. skipping it")
                 continue
 
-            # always grab first the termination_code string
+            # always grab first the termination_code string. if there is no file, skip its summary
             self.MESAsummary.get_termination_code()
+            if "unknown (None)" in self.MESAsummary.termination_code:
+                logger.info(" simulation does not have a termination code. skipping it")
+                continue
 
             # initial conditions of binary system
             if self.stevdb_dict.get("track_initials"):
                 initials_dict = self.__load_history_columns_dict(key="initials")
                 self.MESAsummary.get_initials(history_columns_dict=initials_dict)
 
+                # create table with initials if this is the first model
+                if doing_first_model_of_summary:
+                    create_database(
+                        database_filename=self.database_name,
+                        table_name=self.stevdb_dict.get("id_for_initials_in_database"),
+                        table_dict=self.MESAsummary.Initials,
+                    )
+
+                insert_run_into_database(
+                    database_filename=self.database_name,
+                    table_name=self.stevdb_dict.get("id_for_initials_in_database"),
+                    table_dict=self.MESAsummary.Initials,
+                )
+
             # final conditions of binary system
             if self.stevdb_dict.get("track_finals"):
                 finals_dict = self.__load_history_columns_dict(key="finals")
                 self.MESAsummary.get_finals(history_columns_dict=finals_dict)
 
-            # self.MESAsummary.get_xrb_phase()
+                # create table with finals if this is the first model
+                if doing_first_model_of_summary:
+                    create_database(
+                        database_filename=self.database_name,
+                        table_name=self.stevdb_dict.get("id_for_finals_in_database"),
+                        table_dict=self.MESAsummary.Finals,
+                    )
 
-            print("debugging summary of MESA run")
-            sys.exit(0)
+                # avoid saving None values in database of Finals
+                if all(self.MESAsummary.Finals.values()):
+                    insert_run_into_database(
+                        database_filename=self.database_name,
+                        table_name=self.stevdb_dict.get("id_for_finals_in_database"),
+                        table_dict=self.MESAsummary.Finals,
+                    )
+
+            # before the end of the first evaluation in the for-loop, we set this flag to False in order to avoid
+            # creating the database header again
+            doing_first_model_of_summary = False
+
+            # (tend) to control loading and processing time
+            tend = time.time()
+            logger.debug(f" [loading and processing time of MESA run: {tend-tstart:.2f} sec]")
 
     def __load_history_columns_dict(self, key: str = "") -> dict:
         """Load dictionary with names of MESA history_columns.list to track initial conditions"""
